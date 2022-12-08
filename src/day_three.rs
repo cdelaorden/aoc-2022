de03276cf23b05af36a81fb0225ee9c4778f5012
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-08 02:08:02 +0100

    Feat: Day3 part2

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,6 +1,32 @@
 pub fn sum_priorities(data:String) -> () {
   let rucksacks: Vec<&str> = data.split_terminator('\n').collect();
-  println!("Rucksack 1 {} with length {}", rucksacks[0], rucksacks[0].len());
+  // part one
+  // get_sum_priorities(rucksacks);
+  // part two
+  get_sum_priorities_each_three(rucksacks);
+}
+
+fn get_sum_priorities_each_three (rucksacks: Vec<&str>) {
+  let mut index = 0;
+  let mut repeated_items: Vec<i32> = Vec::new();
+  while index < rucksacks.len() {
+    let first = rucksacks[index];
+    let second = rucksacks[index+1];
+    let third = rucksacks[index+2];
+    for item in first.chars().into_iter() {
+      if second.contains(item) && third.contains(item) {
+        repeated_items.push(item_to_priority(item) as i32);
+        break;
+      }
+    }
+    index += 3;
+    continue;
+  }
+  let result: i32 = repeated_items.iter().sum();
+  println!("Result is {}", result);
+}
+
+fn get_sum_priorities(rucksacks: Vec<&str>) -> () {
   let mut sum_priorities = 0;
   rucksacks.iter().for_each(|rucksack| {
     let repeated_item = find_repeated_in_rucksack(rucksack);
