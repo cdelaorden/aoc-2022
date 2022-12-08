f27b9a1848922df6c46b1c4089963808ad29faaf
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-08 20:07:12 +0100

    Feat: Day 5 part 2, refactor stacks to HashMap

--- src/day_five.rs
+++ src/day_five.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap};
+
 pub fn get_top_of_stacks(data:String) -> () {
   let parts: Vec<&str> = data.split("\n\n").collect();
   let diagram = parts.first().expect("Diagram not found in input file");
@@ -5,31 +7,66 @@ pub fn get_top_of_stacks(data:String) -> () {
   // println!("Diagram {:#?}", diagram);
   let stacks = setup_stacks(diagram);
   let commands = parse_commands(instructions);
-  interpret(stacks, commands);
-  // println!("Instructions {:#?}", parse_commands(instructions));
+  // part 1 
+  // interpret(stacks, commands, false);
+  // part 2
+  interpret(stacks, commands, true);
 }
 
-fn interpret(mut stacks: Vec<Vec<char>>, commands: Vec<Command>) -> () {
+fn interpret(mut stacks: HashMap<String, Vec<char>>, commands: Vec<Command>, move_in_order: bool) -> () {
   let mut result: String = String::new();
   commands.iter().for_each(|cmd| {
-    for _x in 0..cmd.amount {
-      let item = stacks[cmd.from as usize].pop().expect("Stack is empty!");
-      // println!("Moving {} from {} to {}", item, cmd.from, cmd.to);
-      stacks[cmd.to as usize].push(item);
-    }
+    // println!("Executing {:?}", cmd);
+    let mut items_to_append = extract_items(stacks.entry(cmd.from.to_string()).or_default(), cmd.amount as usize, move_in_order);
+    let stack_to = stacks.entry(cmd.to.to_string()).or_default();
+    stack_to.append(&mut items_to_append);
+    // if !move_in_order {
+    //   for _x in 0..cmd.amount {
+    //     let item = stack_from.pop().expect("Stack is empty!");
+    //     // println!("Moving {} from {} to {}", item, cmd.from, cmd.to);
+    //     stack_to.push(item);
+    //   }
+    // }
+    // else {
+    //   let mut items_to_move = stack_from.split_off(stack_from.len() - cmd.amount as usize);
+    //   stack_to.append(&mut items_to_move);
+    //   // let last_index = stacks[cmd.from as usize].len()-1;
+    //   // let items: Vec<char> = stacks[cmd.from as usize][last_index..cmd.amount as usize].into();
+    //   // for item in items.iter(){
+    //   //   stacks[cmd.to as usize].push(*item);
+    //   // }
+    // }  
+
+    // dbg!(&stacks);  
   });
-  for i in 0..stacks.len() {
-    let top = stacks[i].last();
+  for index in 0..stacks.len() {
+    let contents = stacks.get(&index.to_string()).expect("Stack not found");
+    let top = contents.last();
     match top {
-      Some (c) => result += &c.to_string(),
+      Some (c) => result.push(*c),
       None => {}
     }
   }
   println!("Final stack top {}", result)
 }
 
-fn setup_stacks (diagram:&str) -> Vec<Vec<char>> {
-  let mut stacks: Vec<Vec<char>> = Vec::new();
+fn extract_items(vec: &mut Vec<char>, amount: usize, in_order: bool) -> Vec<char> {
+  if in_order {
+    vec.split_off(vec.len() - amount)
+  }
+  else {
+    let mut items:Vec<char> = vec![];
+    for _x in 0..amount {
+      let item = vec.pop().expect("Stack is empty!");
+      // println!("Moving {} from {} to {}", item, cmd.from, cmd.to);
+      items.push(item);
+    }
+    items
+  }
+}
+
+fn setup_stacks (diagram:&str) -> HashMap<String, Vec<char>> {
+  let mut stacks = HashMap::new();
   let mut lines: Vec<&str> = diagram.split("\n").collect();
   let last_line = lines.pop().expect("Invalid stack diagram");
   let max_queue_number = last_line
@@ -39,8 +76,8 @@ fn setup_stacks (diagram:&str) -> Vec<Vec<char>> {
     .map(|x| { u32::from_str_radix(x, 10).expect("Invalid stack number (last)") })
     .expect("Invalid max queue");
 
-  for _i in 0..max_queue_number {
-    stacks.push(Vec::new())
+  for i in 0..max_queue_number {
+    stacks.insert(i.to_string(), Vec::new());
   }
   lines.reverse();
   lines.iter().for_each(|stack_row| {
@@ -51,7 +88,8 @@ fn setup_stacks (diagram:&str) -> Vec<Vec<char>> {
       match code {
           Some (letter) => {
             if letter != ' ' {
-              stacks[stack_index].push(letter)
+              let stack_contents = stacks.entry(stack_index.to_string()).or_insert(Vec::new());
+              stack_contents.push(letter)
             }
           }
           _ => {}
