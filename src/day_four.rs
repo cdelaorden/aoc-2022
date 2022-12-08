9c597dc9d925dc4e5a92e9396a4387d5ed560985
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-08 03:11:28 +0100

    Feat: Day 4 part 2

--- src/day_four.rs
+++ src/day_four.rs
@@ -1,6 +1,5 @@
 type Section = (u32, u32);
 
-
 pub fn camp_cleanup(data:String) -> () {
   let pairs: Vec<(Section, Section)> = data.split_terminator("\n")
   .map(|line| {
@@ -10,15 +9,28 @@ pub fn camp_cleanup(data:String) -> () {
     return (elf1_sections, elf2_sections);
 
   }).collect();
-  let total: i32 = pairs.iter().fold(0, |acc, section_pair|{
+  // part 1
+  let total_fully_contained: i32 = pairs.iter().fold(0, |acc, section_pair|{    
     if is_contained(section_pair.0, section_pair.1) || is_contained(section_pair.1, section_pair.0) {
+      // println!("Overlap found {:#?} {:#?}", section_pair.0, section_pair.1);
       acc + 1
     }
     else {
       acc
     }
   });
-  println!("Total fully contained {}", total);
+  println!("Part 1 - Total fully contained {}", total_fully_contained);
+  // part 2
+  let total_overlapping: i32 = pairs.iter().fold(0, |acc, section_pair|{    
+    if overlaps(section_pair.0, section_pair.1) || overlaps(section_pair.1, section_pair.0) {
+      // println!("Overlap found {:#?} {:#?}", section_pair.0, section_pair.1);
+      acc + 1
+    }
+    else {
+      acc
+    }
+  });
+  println!("Part 2 - Total overlapping {}", total_overlapping);
 }
 
 fn parse_section (x: &str) -> (u32, u32) {
@@ -29,4 +41,8 @@ fn parse_section (x: &str) -> (u32, u32) {
 
 fn is_contained (s: Section, in_section: Section) -> bool {
   return s.0 >= in_section.0 && s.1 <= in_section.1
+}
+
+fn overlaps (s: Section, in_section: Section) -> bool {
+  return s.0 <= in_section.1 && in_section.0 <= s.1;
 }
