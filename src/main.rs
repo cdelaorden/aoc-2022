1199fa36db68eeec06fa82e86f2b4d341ea9fdc5
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-08 02:21:23 +0100

    Chore: Remove dead code warn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+#![allow(dead_code)]
 mod input;
 mod day_one;
 mod day_two;
 mod day_three;
+mod day_four;
+
+
 fn main() {
     let data = input::read_input_file();   
     // day_one::get_elves_calories(data);
     // day_two::get_total_score(data);
-    day_three::sum_priorities(data);
+    // day_three::sum_priorities(data);
+    day_four::camp_cleanup(data);
     
     
 }
