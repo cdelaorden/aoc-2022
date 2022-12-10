59c04f49c7ca14023586fbf81e8e9d9fa3d17686
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-10 18:24:08 +0100

    Feat: Day 10 part 2: CRT screen

--- src/day_ten.rs
+++ src/day_ten.rs
@@ -9,51 +9,34 @@ enum Instruction {
 
 pub fn cathode_ray_tube(data:&str) {
   let instructions = parse_instructions(data);
-  // println!("Program {:?}", instructions);
-  println!("Output after 20 cycles {}", run(&instructions, 20));
+  // println!("Program {:?}", instructions);  
+  println!("Output after 20 cycles {}", run(&instructions, 20).0);
   println!("Signal strength {}", get_signal_strength_sum(&instructions));
   println!("--------");
-  draw_crt(&instructions);
+  let (_, crt) = run(&instructions, 240);
+  println!("{}", crt);
 } 
 
 fn get_signal_strength_sum (instructions:&Vec<Instruction>) -> i32 {
   let sample_at = [20, 60, 100, 140, 180, 220];
   let mut strengths = [0, 0, 0, 0, 0, 0];
   for (i, cycles) in sample_at.iter().enumerate() {
-    let strength = run(instructions, *cycles);
+    let (strength, _crt) = run(instructions, *cycles);
     strengths[i] = strength * *cycles as i32;
   }
   strengths.iter().sum()
 }
 
-fn draw_crt (instructions:&Vec<Instruction>) -> () {
-  let mut draw_index = 0;
-  let mut pixels: Vec<char> = Vec::new();
-  for cycle in 0..220 {
-    let x = run(instructions, cycle);
-    let sprite = [x-1, x, x+1];
-    println!("Drawing: {}. X: {:?}", draw_index, sprite);
-    if sprite.contains(&(&draw_index % 40)) {
-      pixels.push('#')
-    }
-    else {
-      pixels.push('.')  
-    }    
-    draw_index += 1;
-  }
-  // draw lines
-  for i in 0..5 {
-    println!("{}", &String::from_iter(pixels[i*40..(i+1)*40].iter()));   
-  }
-}
 
-fn run(instructions:&Vec<Instruction>, num_cycles:u32) -> i32 {
+
+fn run(instructions:&Vec<Instruction>, num_cycles:u32) -> (i32, String) {
   let mut x: i32 = 1;
   let mut current_index = 0;
   let mut wait = 0;
   let mut next_value: i32 = 1;
-  for _i in 0..num_cycles {
-    // println!("Cycle {}. X: {}. Wait: {}. Next: {}", _i, x, wait, next_value);
+  let mut crt = String::from("");
+  for i in 0..num_cycles {
+    let pixel_x = i as i32 % 40;
     if wait > 0 {
       wait -= 1;      
     }
@@ -71,8 +54,18 @@ fn run(instructions:&Vec<Instruction>, num_cycles:u32) -> i32 {
       }
       current_index += 1;
     }
+    // draw to screen
+    if pixel_x as i32 == x || pixel_x as i32  == x-1 || pixel_x as i32 == x+1 {
+      crt.push('#');
+    }
+    else {
+      crt.push('.');
+    }
+    if i > 0 && (i+1) % 40 == 0 {
+      crt.push('\n');
+    }
   }
-  x
+  (x, crt)
 }
 
 fn parse_instructions(data:&str) -> Vec<Instruction> {
