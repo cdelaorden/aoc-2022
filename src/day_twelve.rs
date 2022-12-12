f0867137b905214900ae5c2e32d9212b7336757c
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-12 23:23:14 +0100

    Feat: Day 12 part one

--- src/day_twelve.rs
+++ src/day_twelve.rs
@@ -1,42 +1,114 @@
+use std::collections::{HashSet, VecDeque};
+
 pub fn hill_climbing_algorithm (data: &str) {
-  let map = parse_map(&data);
-  println!("Map {:#?}", map);
+  let map = parse(&data);
+  // println!("Map {:#?}", map);
+  // // part 1
+  let min_distance = find_path(&map);
+  println!("Min distance is {:?}", min_distance);
 }
 
-#[derive(Debug)]
-struct Map {
-  grid: Vec<Vec<u32>>,
-  start_pos: (u32, u32),
-  end_pos: (u32, u32)
-}
+fn find_path(
+  map: &HeightMap
 
+) -> Option<usize> {
+  let mut visited: HashSet<Point> = HashSet::new();
+  
+  let mut queue = VecDeque::new();
+  let current = map.start;
+  queue.push_back((current, Vec::new()));
 
-fn parse_map (data: &str) -> Map {
-  let mut map = Map {
-    grid: vec![],
-    start_pos: (0,0),
-    end_pos: (0,0)
+  let is_allowed = |to_height:u8, from_height:u8| {
+    to_height <= from_height + 1
   };
-  data
-    .split_terminator("\n")
-    .enumerate()
-    .for_each(|(x, line)|{
-      let mut row = vec![];
-      line.chars().enumerate().
-      for_each(|(y, c)|{
-        match c {
-          'S' => {
-            map.start_pos = (x.try_into().unwrap(), y.try_into().unwrap());
-            row.push('a' as u32);
-          }
-          'E' => {
-            map.end_pos = (x.try_into().unwrap(), y.try_into().unwrap());
-            row.push('z' as u32);
-          }
-          _ => row.push(c as u32)
-        }
-      });
-      map.grid.push(row);      
-    });    
-    map
+
+  while !queue.is_empty() {
+    let (current, history) = queue.pop_front().unwrap();
+    if (current == map.end) {
+      return Some(history.len());
+    }
+    // println!("Checking {:?} with history {:?}", current, history);
+
+    let current_height = map.data[current.y][current.x];
+
+    let mut check_neighbour = |x:usize, y:usize| {
+      if x >= map.data[0].len()
+        || y >= map.data.len()
+        || !is_allowed(map.data[y][x], current_height)
+        || visited.contains(&Point { x, y })
+      {
+        return;  
+      }
+      visited.insert(Point::new(x, y));
+      let mut new_history = history.clone();
+      new_history.push(current);
+      queue.push_back((Point::new(x, y), new_history));
+    };
+
+    check_neighbour(current.x + 1, current.y);
+    check_neighbour(current.x.wrapping_sub(1), current.y);
+    check_neighbour(current.x, current.y + 1);
+    check_neighbour(current.x, current.y.wrapping_sub(1));
+  }
+
+  None
+}
+
+#[derive(Debug, Clone)]
+struct HeightMap {
+  data: Vec<Vec<u8>>,
+  start: Point,
+  end: Point
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
+  x: usize,
+  y: usize
+}
+impl Point {
+  fn new(x: usize, y: usize) -> Self {
+      Self { x, y }
+  }
+}
+
+fn parse(data:&str) -> HeightMap {
+  let mut out = Vec::new();
+  let mut start = Point::new(0, 0);
+  let mut end = Point::new(0, 0);
+
+  for i in data.lines() {
+    let mut row = Vec::new();
+    for j in i.chars() {
+      match j {
+        'S' => {
+          row.push(0);
+          start = Point::new(row.len() - 1, out.len());
+        },
+        'E' => {
+          row.push(25);
+          end = Point::new(row.len() - 1, out.len());
+        },
+        _ => row.push(j as u8 - 97)
+      }
+    }
+    out.push(row);
+  }
+  HeightMap { data: out, start: start, end: end }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  #[test]
+  fn it_parses_height_map () {
+    let m = parse("Sabqponm\n\
+    abcryxxl\n\
+    accszExk\n\
+    acctuvwj\n\
+    abdefghi");
+    // S is 0 (a)
+    assert_eq!(m.data[0][0], 0);
+    // E is 25 (z)
+    assert_eq!(m.data[2][4], 'z' as u8 - 97);
+  }
 }
