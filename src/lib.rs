2fb657ab35fd4a309887914e7cc22a503fae451c
Carlos de la Orden <cdelaorden@gmail.com>
2022-12-13 00:49:53 +0100

    Chore: Move inputs to 2022 subfolder

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,13 @@ pub fn read_input_file() -> (u32, String) {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {        
         println!("Call with `cargo run -- <day> [<path_to_input>]");
-        println!("If ommited, <path_to_input> defaults to inputs/<day>.txt (puzzle input)");
+        println!("If ommited, <path_to_input> defaults to inputs/2022/<day>.txt (puzzle input)");
         std::process::exit(1);
     }
     let day_number = u32::from_str_radix(&args[1], 10).expect("Invalid day");
     let filename = env::args()
         .nth(2)
-        .or_else(|| Some(format!("inputs/{day_number}.txt")))
+        .or_else(|| Some(format!("inputs/2022/{day_number}.txt")))
         .unwrap();
     let data = fs::read_to_string(&filename);
     if data.is_err() {
